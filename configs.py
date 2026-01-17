5724b7b7045d9b67fd9c4734debdd510fa784fbc
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-01-17 20:16:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,20 +1,34 @@
-
-
-
-from os import path, getenv
+from os import getenv
 
 class Config:
     API_ID = int(getenv("API_ID", "21419016"))
     API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8455395512:AAGbNe2nv9_ztC685urGaGLIeDH5hQdJIRQ")
-    # Your Force Subscribe Channel Id Below=
-    CHID = int(getenv("CHID", "-1003431327885")) # Make Bot Admin In This Channel
-    # Admin Or Owner Id |
-    SUDO = list(map(int, getenv("SUDO", "7554081592 7001232146 7564050858 5656436152").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://melodysotto4_db_user:BCUKIKDEAqFEzeCj@cluster0.trrt89o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "xxxxxxxxxxxxxxx")
+
+    # Admin / Owner IDs
+    SUDO = list(map(int, getenv(
+        "SUDO",
+        "7554081592 xxxxxxxxxxxxxx 7564050858 5656436152"
+    ).split()))
+
+    MONGO_URI = getenv(
+        "MONGO_URI",
+        "mongodb+srv://melodysotto4_db_user:BCUKIKDEAqFEzeCj@cluster0.trrt89o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+
+    # Posts to copy
     POSTS = [
-        "https://t.me/forward_hack_lnx/3",
-        "https://t.me/forward_hack_lnx/4"
+        "https://t.me/forward_hack_lnx/8",
+        "https://t.me/forward_hack_lnx/10"
+    ]
+
+    # 🚫 ILLEGAL WORDS (BOT SIDE FILTER)
+    ILLEGAL_WORDS = [
+        "@controllerbot",
+        "creatings",
+        "tasks",
+        "tasks.",
+        "accomplish"
     ]
 
 cfg = Config()
